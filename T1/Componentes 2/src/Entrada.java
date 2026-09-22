d85ea510789de0d2f6dfc0dc1f08b19690a1a2a9
TyRooZh <leyrecucullogamboa@gmail.com>
2026-09-22 14:01:49 +0200

    Estructura T1 organizada y añadida a Dam-programacion

---
+++ T1/Componentes 2/src/Entrada.java
@@ -0,0 +1,29 @@
+/**
+ * @author leyre
+ * @version 1.0
+ * @
+ */
+public class Entrada {
+    // aqui explicas brevemente algo para aclarate las ideas
+    // en esta duda hago una aclaracion diferente
+    // otra aclaracion
+    /*
+    Este comentario admite unas cuantas lineas
+    Esta es la segunda linea
+     */
+    // TODO esta tarea la dejo mpendiente para el lunes
+
+
+    public static void main(String [] args) {
+        System.out.print("Hola Mundo");
+    }
+    /**
+     *
+     * @param arg explica el parametro
+     * @return explica el retono
+     */
+    public int metodo(String arg) {
+        return 1;
+    }
+}
+
