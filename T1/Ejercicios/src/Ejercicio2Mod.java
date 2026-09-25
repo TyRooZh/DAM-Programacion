68d8799edb215d882657f01ef24737718e5a1162
TyRooZh <leyrecucullogamboa@gmail.com>
2026-09-25 10:48:31 +0200

    Actualizacion ejercicios

---
+++ T1/Ejercicios/src/Ejercicio2Mod.java
@@ -0,0 +1,15 @@
+public class Ejercicio2Mod {
+    public static void main(String[] args){
+        int inicial = 0;
+        System.out.println("Puntuación inicial: "+inicial);
+
+        inicial = 5;
+        System.out.println("Después de primera modificación: "+inicial);
+
+        inicial = 10;
+        System.out.println("Despues de segunda modificación: "+inicial);
+
+        inicial = 15;
+        System.out.println("Puntuación final: "+inicial);
+    }
+}
--- T1/Ejercicios/src/Ejercicio5.java
+++ T1/Ejercicios/src/Ejercicio5.java
@@ -6,10 +6,35 @@ public class Ejercicio5 {
         String version = "1.0.0";
         final double PI = 3.14159;
         Scanner lector = new Scanner(System.in);
-        
 
+        System.out.println("Aplicación: "+APLICACION);
+        System.out.println("Versión: "+version);
+        System.out.println("Valor de PI: "+PI);
 
+        System.out.println("Introduce tu nombre");
+        String nombre = lector.nextLine();
+        System.out.println("Nombre actual: "+nombre);
 
+        int nivel = 1;
+        System.out.println("Nivel: "+nivel);
 
+        System.out.println("Introduzca su puntuación");
+        int puntuacion = lector.nextInt();
+        lector.nextLine();
+        System.out.println("Puntuación: "+puntuacion);
+
+        System.out.println("Introduce tu nombre");
+        nombre = lector.nextLine();
+        System.out.println("Nombre actualizado: "+nombre);
+
+        int nivel1 = 2;
+        System.out.println("Nivel: "+nivel1);
+
+        System.out.println("Introduzca su puntuación");
+        puntuacion = lector.nextInt();
+        lector.nextLine();
+        System.out.println("Puntuación: "+puntuacion);
+
+        lector.close();
     }
 }
